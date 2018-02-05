ca523af7dd7cba10cdaa2935a6c7f1fa4e5453ab
Ishan <ishan.sawant95@gmail.com>
2018-02-05 16:32:12 +1100

    added draw_from_source function to speed up drawing function. Canvas size was defaulting to 300 x 150, code forces the canvas to resize by calling self.canvas = Canvas(self, resolution=(100, 100)) before appending canvas

--- remi/game/raster.py
+++ remi/game/raster.py
@@ -18,17 +18,45 @@ def load_image(image_file):
     result = RasterImage()
     result.width = image.size[0]
     result.height = image.size[1]
+    print("Loading image, width is: " + str(result.width) +
+          " height is: " + str(result.height))
     result.data = repr(data)
     return result
 
 
-def draw(image, canvas, position):
+def draw(image, canvas, position): #Draws pixel by pixel - much slower to draw
     canvas.draw('''var canvas = document.getElementById('%s');
+canvas.width = %s;
+canvas.height = %s;
 var ctx = canvas.getContext('2d');
 var image = ctx.createImageData(%s, %s);
-image.data.set(new Uint8ClampedArray(%s));
+image.data.set(new Uint8Clamped Array(%s));
 ctx.putImageData(image, %s, %s);''' % (
-    canvas.id,
-    image.width, image.height, image.data,
-    position[0], position[1]
+        canvas.id,
+        canvas.style['width'].replace('px', ''),
+        canvas.style['height'].replace('px', ''),
+        image.width, image.height, image.data,
+        position[0], position[1],
+        canvas.id,
+        image.width, image.height,
+        position[0], position[1],  # image.data,
+    ))
+
+
+def draw_from_source(canvas, img_source, position, width, height):
+    #Uses preloaded image so it draws faster
+    #print(canvas.style)
+    canvas.draw('''var canvas = document.getElementById('%s');
+canvas.width = %s;
+canvas.height = %s;
+var ctx = canvas.getContext('2d');
+var image = document.getElementById('%s');
+ctx.drawImage(image, %s, %s, %s, %s);''' % (
+        canvas.id,
+        canvas.style['width'].replace('px', ''),
+        canvas.style['height'].replace('px', ''),
+        #forces default width and height to be replaced with user inputted
+        img_source.identifier,
+        position[0], position[1],
+        width, height
     ))
